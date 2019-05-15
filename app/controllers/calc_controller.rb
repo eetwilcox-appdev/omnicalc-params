d2dea0a57386ebcd1ee67205c23c06fcaa1df6dc
Ellen Wilcox <ellen.wilcox@kellogg.northwestern.edu>
2019-05-15 05:27:46 +0000

    Square form and results RCAVs both working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -7,5 +7,21 @@ class CalcController < ApplicationController
         
         render("flex_templates/square.html.erb")
     end
+        
+    def flex_pmt
+        render("flex_templates/pmt.html.erb")
+    end
+    
+    def square_form
+        render("form_templates/square.html.erb")
+    end
+    
+    def process_square_form
+        
+        @num = params.fetch("user_number").to_f
+        @square_of_num = @num ** 2
+        
+        render("results_templates/square.html.erb")
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get" })
-  
+  match("/flexible/payment/:zebra/:giraffe/:elephant", { :controller => "calc", :action => "flex_pmt", :via => "get" })
+  match("/square/new", { :controller => "calc", :action => "square_form", :via => "get" })
+  match("/square/results", { :controller => "calc", :action => "process_square_form", :via => "get" })
 end
