1ae6f002263d73008754c4b78580843db8380967
Ellen Wilcox <ellen.wilcox@kellogg.northwestern.edu>
2019-05-16 01:57:06 +0000

    Square root form works

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -7,9 +7,38 @@ class CalcController < ApplicationController
         
         render("flex_templates/square.html.erb")
     end
+    
+    def flex_square_root
+        
+        @a_number = params.fetch("zebra").to_i
+        @square_root = @a_number ** (0.5)
+        
+        render("flex_templates/sqrroot.html.erb")
+    end
         
     def flex_pmt
+        
+        @basis_points = params.fetch("basis_points").to_f
+        @monthly_rate = @basis_points/12
+        @monthly_rate_decimal = @monthly_rate/10000
+        @rate = @basis_points/100
+        
+        @number_of_years = params.fetch("number_of_years").to_i
+        @number_of_periods = @number_of_years*12
+        
+        @present_value = params.fetch("present_value").to_i
+        
+        @monthly_payment = (@monthly_rate_decimal*@present_value)/(1-((1+@monthly_rate_decimal)**-@number_of_periods))
+        
         render("flex_templates/pmt.html.erb")
+    end
+    
+    def flex_random
+        
+        @max = params.fetch("max").to_i
+        @min = params.fetch("min").to_i
+        
+        render("flex_templates/random.html.erb")
     end
     
     def square_form
@@ -22,6 +51,18 @@ class CalcController < ApplicationController
         @square_of_num = @num ** 2
         
         render("results_templates/square.html.erb")
+    end
+    
+    def square_root_form
+        render("form_templates/square_root.html.erb")
+    end
+    
+    def process_square_root_form
+        
+        @num_to_sqr = params.fetch("user_number_to_sqr").to_f
+        @sqrt_of_num = @num_to_sqr ** 0.5
+        
+        render("results_templates/square_root.html.erb")
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get" })
-  match("/flexible/payment/:zebra/:giraffe/:elephant", { :controller => "calc", :action => "flex_pmt", :via => "get" })
+  match("/flexible/square_root/:zebra", { :controller => "calc", :action => "flex_square_root", :via => "get" })
+  match("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calc", :action => "flex_pmt", :via => "get" })
+  match("/flexible/random/:min/:max", { :controller => "calc", :action => "flex_random", :via => "get" })
+  
   match("/square/new", { :controller => "calc", :action => "square_form", :via => "get" })
   match("/square/results", { :controller => "calc", :action => "process_square_form", :via => "get" })
+  
+  match("/square_root/new", { :controller => "calc", :action => "square_root_form", :via => "get" }) 
+  match("/square_root/results", { :controller => "calc", :action => "process_square_root_form", :via => "get" }) 
+  
+   match("/payment/new", { :controller => "calc", :action => "", :via => "get" })
+  
 end
